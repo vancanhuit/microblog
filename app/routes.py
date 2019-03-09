1713490e23db22f358ac21572f3d7220a072ac60
vancanhuit <vancanhuit@gmail.com>
2019-03-09 19:54:03 +0700

    Working with form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import (
+    render_template, flash, redirect, url_for)
 from app import application
+from app.forms import LoginForm
 
 
 @application.route('/')
@@ -18,3 +20,13 @@ def index():
         }
     ]
     return render_template('index.html', user=user, posts=posts)
+
+
+@application.route('/login', methods=('GET', 'POST'))
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remmeber_me = {}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
