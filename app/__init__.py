68fd1072e38adc088cef6c16282dba962ee96026
vancanhuit <vancanhuit@gmail.com>
2019-03-10 09:18:27 +0700

    Working with database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 application = Flask(__name__)
 application.config.from_object(Config)
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
 
 from app import routes
+from app import models
