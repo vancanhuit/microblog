492f26e918e620023a4225f82d03cf984106c5f1
vancanhuit <vancanhuit@gmail.com>
2019-03-10 19:03:25 +0700

    Error handling

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from config import Config
 
 application = Flask(__name__)
@@ -11,5 +14,39 @@ migrate = Migrate(application, db)
 login_manager = LoginManager(application)
 login_manager.login_view = 'login'
 
-from app import routes
-from app import models
+if not application.debug:
+    if application.config['MAIL_SERVER']:
+        auth = None
+        if application.config['MAIL_USERNAME'] or \
+                application.config['MAIL_PASSWORD']:
+            auth = (
+                application.config['MAIL_USERNAME'],
+                application.config['MAIL_PASSWORD'])
+        secure = None
+        if application.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(
+                application.config['MAIL_SERVER'],
+                application.config['MAIL_PORT']),
+            fromaddr='no-reply@' + application.config['MAIL_SERVER'],
+            toaddrs=application.config['ADMINS'],
+            subject='Miroblog Failure', secure=secure,
+            credentials=auth
+        )
+        mail_handler.setLevel(logging.ERROR)
+        application.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler(
+        'logs/app.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    application.logger.addHandler(file_handler)
+
+    application.logger.setLevel(logging.INFO)
+    application.logger.info('Miroblog startup')
+
+from app import routes, models, errors
