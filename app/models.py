b98a3307f7b8f66732c59561f0bc25e534c8065b
vancanhuit <vancanhuit@gmail.com>
2019-03-17 12:48:29 +0700

    Add api auth

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from flask import current_app, url_for
-from datetime import datetime
+from datetime import datetime, timedelta
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 from time import time
 import jwt
+import base64
+import os
 from app import db, login_manager
 from app.search import add_to_index, remove_from_index, query_index
 
@@ -93,6 +95,8 @@ class User(db.Model, UserMixin, PagniatedAPIMixin):
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(300))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     followed = db.relationship(
@@ -177,6 +181,25 @@ class User(db.Model, UserMixin, PagniatedAPIMixin):
                 setattr(self, field, data[field])
         if new_user and 'password' in data:
             self.set_password(data['password'])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
